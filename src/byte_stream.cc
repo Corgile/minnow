5254e9c980de07aed743b917f395d3db0e508d85
corgile <mashiro-e@qq.com>
2024-10-08 11:39:46 +0800

    [ADD] 实现 byte_stream

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -2,64 +2,80 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t const capacity ) : capacity_( capacity ) {}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return closed_;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
-  return;
+  if ( is_closed() or available_capacity() == 0 or data.empty() ) {
+    return;
+  }
+  if ( data.size() > available_capacity() ) {
+    data.resize( available_capacity() );
+  }
+  bytes_pushed_ += data.size();
+  bytes_available_ += data.size();
+
+  buffers_.emplace_back( std::move( data ) );
 }
 
 void Writer::close()
 {
-  // Your code here.
+  closed_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_ - bytes_available_;
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return bytes_pushed_;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  return closed_ and bytes_available_ == 0;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return bytes_popped_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  if ( buffers_.empty() ) {
+    return {};
+  }
+  auto const& value = buffers_.front();
+  // 不用一次性全部peek出去
+  return { value.data() + read_prefix_, value.length() - read_prefix_ };
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  len = std::min( len, bytes_available_ );
+  bytes_available_ -= len;
+  bytes_popped_ += len;
+  while ( len != 0LU ) {
+    std::size_t const size { buffers_.front().size() - read_prefix_ };
+    if ( len < size ) {
+      read_prefix_ += len;
+      break; // with len = 0;
+    }
+    buffers_.pop_front();
+    read_prefix_ = 0;
+    len -= size;
+  }
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return bytes_available_;
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <deque>
 #include <string>
 #include <string_view>
 
@@ -14,17 +15,23 @@ public:
 
   // Helper functions (provided) to access the ByteStream's Reader and Writer interfaces
   Reader& reader();
-  const Reader& reader() const;
   Writer& writer();
-  const Writer& writer() const;
+  void set_error() { error_ = true; } // Signal that the stream suffered an error.
 
-  void set_error() { error_ = true; };       // Signal that the stream suffered an error.
-  bool has_error() const { return error_; }; // Has the stream had an error?
+  [[nodiscard]] const Reader& reader() const;
+  [[nodiscard]] const Writer& writer() const;
+  [[nodiscard]] bool has_error() const { return error_; } // Has the stream had an error?
 
 protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
-  uint64_t capacity_;
   bool error_ {};
+  bool closed_ {};
+  std::size_t capacity_;
+  std::size_t read_prefix_ {};
+  std::size_t bytes_popped_ {};
+  std::size_t bytes_pushed_ {};
+  std::size_t bytes_available_ {};
+  std::deque<std::string> buffers_ {};
 };
 
 class Writer : public ByteStream
@@ -33,24 +40,24 @@ public:
   void push( std::string data ); // Push data to stream, but only as much as available capacity allows.
   void close();                  // Signal that the stream has reached its ending. Nothing more will be written.
 
-  bool is_closed() const;              // Has the stream been closed?
-  uint64_t available_capacity() const; // How many bytes can be pushed to the stream right now?
-  uint64_t bytes_pushed() const;       // Total number of bytes cumulatively pushed to the stream
+  [[nodiscard]] bool is_closed() const;              // Has the stream been closed?
+  [[nodiscard]] uint64_t available_capacity() const; // How many bytes can be pushed to the stream right now?
+  [[nodiscard]] uint64_t bytes_pushed() const;       // Total number of bytes cumulatively pushed to the stream
 };
 
 class Reader : public ByteStream
 {
 public:
-  std::string_view peek() const; // Peek at the next bytes in the buffer
-  void pop( uint64_t len );      // Remove `len` bytes from the buffer
+  void pop( uint64_t len ); // Remove `len` bytes from the buffer
 
-  bool is_finished() const;        // Is the stream finished (closed and fully popped)?
-  uint64_t bytes_buffered() const; // Number of bytes currently buffered (pushed and not popped)
-  uint64_t bytes_popped() const;   // Total number of bytes cumulatively popped from stream
+  [[nodiscard]] std::string_view peek() const;   // Peek at the next bytes in the buffer
+  [[nodiscard]] bool is_finished() const;        // Is the stream finished (closed and fully popped)?
+  [[nodiscard]] uint64_t bytes_buffered() const; // Number of bytes currently buffered (pushed and not popped)
+  [[nodiscard]] uint64_t bytes_popped() const;   // Total number of bytes cumulatively popped from stream
 };
 
 /*
- * read: A (provided) helper function thats peeks and pops up to `len` bytes
+ * read: A (provided) helper function that peeks and pops up to `len` bytes
  * from a ByteStream Reader into a string;
  */
 void read( Reader& reader, uint64_t len, std::string& out );
